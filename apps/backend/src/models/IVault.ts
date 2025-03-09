72aa7dc67ec21c77d0b3f3e8460ecc9a334c4816
Haroon Saifi <haroonsaifi2001@gmail.com>
2025-03-09 16:16:43 +0530

    fix

---
+++ apps/backend/src/models/IVault.ts
@@ -0,0 +1,22 @@
+export interface IVault {
+  title: string;
+  img: string;
+  lockedInPeriod: number;
+  APR: number;
+  type: "fixed" | "flexible";
+  earnings: number;
+  proxyAddress: string;
+  tokenAddress: string;
+  AirdropIncentivised: number;
+  totalSupply: number;
+  availableSupply: number;
+  joinInPeriod: string;
+  claimInPeriod: string;
+  price: number;
+  yieldValue: number;
+  backingRatio: number;
+  backingPercentage: number;
+  tokenSymbol: string;
+  NFTAddress: string;
+  NFTLimit: number;
+}
--- apps/backend/src/models/vault.ts
+++ apps/backend/src/models/vault.ts
@@ -1,27 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
-export interface IVault {
-  title: string;
-  img: string;
-  lockedInPeriod: number;
-  APR: number;
-  type: "fixed" | "flexible";
-  earnings: number;
-  proxyAddress: string;
-  tokenAddress: string;
-  AirdropIncentivised: number;
-  totalSupply: number;
-  availableSupply: number;
-  joinInPeriod: string;
-  claimInPeriod: string;
-  price: number;
-  yieldValue: number;
-  backingRatio: number;
-  backingPercentage: number;
-  tokenSymbol: string;
-  NFTAddress: string;
-  NFTLimit: number;
-}
+import { IVault } from "./IVault";
 
 const vaultSchema = new Schema<IVault>({
   title: { type: String, required: true },
