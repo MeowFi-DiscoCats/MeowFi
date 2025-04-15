e13488db0d79be31bddb4216c5ac1838bea9d860
kshaan-ali <kshaaneali@gmail.com>
2025-04-15 18:59:05 +0530

    live for sharkTank

--- apps/frontend/src/lib/default.ts
+++ apps/frontend/src/lib/default.ts
@@ -52,31 +52,32 @@ export const CurvanceAndFastlane: IVault = {
   tokenAbi: shMonadAbi,
   nftImage:'/images/hello.png'
 };
-// export const CurvanceAndFastlane2: IVault = {
-//   //testing purpose
-//   title: 'Curvance x Fastlane',
-//   img: 'https://plum-imaginative-guan-725.mypinata.cloud/ipfs/bafkreifkhhgyhv5f3bhbegnoc3shbmjoo72ngvqntadgyi7wcq2rbiyuxi',
-//   lockedInPeriod: 0,
-//   APR: 225,
-//   type: 'flexible',
-//   earnings: 0,
-//   proxyAddress: '0x5b52Aa26838A1A25Bea959f420b6C1cF2Da7C7f3',
-//   tokenAddress: shMonadErc20Addr,
-//   AirdropIncentivised: 0,
-//   totalSupply: 10000,
-//   availableSupply: 0,
-//   joinInPeriod: '2025-03-15T00:00',
-//   claimInPeriod: '2025-03-17T00:00',
-//   price: 0.2,
-//   yieldValue: 0,
-//   backingRatio: 1,
-//   backingPercentage: 100,
-//   tokenSymbol: 'shMON',
-//   NFTLimit: 1,
-//   isErc20: true,
-//   abi: erc20NftTimeVaultCurvance,
-//   tokenAbi: shMonadAbi,
-// };
+export const CurvanceAndFastlane2: IVault = {
+  //testing purpose
+  title: 'Curvance x Fastlane',
+  img: 'https://plum-imaginative-guan-725.mypinata.cloud/ipfs/bafkreifkhhgyhv5f3bhbegnoc3shbmjoo72ngvqntadgyi7wcq2rbiyuxi',
+  lockedInPeriod: 0,
+  APR: 225,
+  type: 'flexible',
+  earnings: 0,
+  proxyAddress: '0xCFE6376Ec9E56a92E269d0aDE242020759b2c707',
+  tokenAddress: shMonadErc20Addr,
+  AirdropIncentivised: 0,
+  totalSupply: 10000,
+  availableSupply: 0,
+  joinInPeriod: '2025-03-15T00:00',
+  claimInPeriod: '2025-03-17T00:00',
+  price: 0.2,
+  yieldValue: 0,
+  backingRatio: 1,
+  backingPercentage: 100,
+  tokenSymbol: 'shMON',
+  NFTLimit: 1,
+  isErc20: true,
+  abi: erc20NftTimeVaultCurvance,
+  tokenAbi: shMonadAbi,
+  nftImage:'/images/hello.png'
+};
 export const shMon: IErc20 = {
   title: 'shMon',
   img: '/images/monad.webp',
@@ -96,5 +97,5 @@ export const usdc: IErc20 = {
   tokenAbi: tokenAbi,
 };
 
-export const dataArr: IVault[] = [CurvanceAndFastlane];
+export const dataArr: IVault[] = [CurvanceAndFastlane,CurvanceAndFastlane2];
 export const erc20Arr: IErc20[] = [shMon, usdt,usdc];
